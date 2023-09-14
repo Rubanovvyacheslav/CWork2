94ededfbaf31d1dc0fb49d7cd947ea99a49fc815
Rubanovvyacheslav <Rubanov.work@gmail.com>
2023-09-14 21:20:36 +0300

    Initial commit

---
+++ src/test/java/pro/sky/CWork2/CWork2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.CWork2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CWork2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
